d5fe915e634583ed1d2371341ddc1f0b10c5c509
GrMorality <jmihailys@gmail.com>
2024-06-10 03:22:59 +1000

    added: docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 def capitalize_string():
+    """
+    Функция возвращает изменённую строку,
+    где первая буква каждого слова написана заглавными буквами.
+    input_string — строка, которую нужно обработать
+    """
     words = input().split()
     modified_words = [word.capitalize() for word in words]
     return ' '.join(modified_words)
